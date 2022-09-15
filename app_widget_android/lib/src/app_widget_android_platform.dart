42f868ebfb6fdad2ba4ed8909c67285a8f031791
Alexander Dischberg <noxasch@gmail.com>
2022-09-16 05:12:35 +0800

    fix(android): registerWith cannot have methodchannel

--- app_widget_android/lib/src/app_widget_android_platform.dart
+++ app_widget_android/lib/src/app_widget_android_platform.dart
@@ -1,10 +1,10 @@
-import 'package:app_widget_android/app_widget_android.dart';
 import 'package:app_widget_platform_interface/app_widget_platform_interface.dart';
 
 class AppWidgetAndroid extends AppWidgetPlatform {
   // this is use by flutter pluginRegistrant
   // this will register with no callback
+  // no methodChannel are allowed since this are call before the app
   static void registerWith() {
-    AppWidgetPlatform.instance = AppWidgetAndroidPlugin();
+    AppWidgetPlatform.instance = AppWidgetAndroid();
   }
 }
